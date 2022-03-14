4f9176b1be72c8e5da81a84c38b5f902f5eb6258
Mark Anderson <markanderson@wtmbpmaanderson.lan>
2022-03-14 10:18:29 -0400

    added solution for automated test

--- app/src/androidTest/java/com/willowtree/vocable/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/ExampleInstrumentedTest.kt
@@ -1,5 +1,9 @@
 package com.willowtree.vocable
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -28,5 +32,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.willowtree.vocable.debug", appContext.packageName)
+        onView(withId(R.id.category_back_button)).check(matches(isCompletelyDisplayed()))
     }
 }
